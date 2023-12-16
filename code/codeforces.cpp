a7a096aec3655a76b4fc71a74a82f56f3c5c6c34
jainilbavishi24 <bavishijainil24@gmail.com>
2023-12-16 11:02:20 +0530

    Final code

--- code/codeforces.cpp
+++ code/codeforces.cpp
@@ -50,6 +50,7 @@ void solve()
     i = 0;
     lli k = 0;
     string str = s2;
+    string b = s2;
     lli ans = 2 * frontcnt - 1;
     if (frontcnt <= 1)
     {
@@ -61,7 +62,37 @@ void solve()
         cout << 0 << endl;
         return;
     }
+    else
+    {
+        reverse(b.begin(), b.end());
+        if (frontcnt % 2 == 0)
+        {
+            if (str == b)
+            {
+            }
+            else
+            {
+                ans++;
+            }
+        }
+        lli cnt = 2 * reversecnt - 1;
+        if (reversecnt % 2)
+        {
+            if (str == b)
+            {
+            }
+            else
+            {
+                cnt++;
+            }
+        }
+        ans = min(ans, cnt);
+    }
+    
+    cout << ans << endl;
+    return;
 }
+
 int main()
 {
     ios_base::sync_with_stdio(false);
